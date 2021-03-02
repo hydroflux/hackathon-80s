de5d2d8f26b8ec4d4e6a31f4f0b52a04e00405fd
Jack Hubert <jphubert93@gmail.com>
2021-03-02 11:17:54 -0700

    worked with last fm api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  apiKey = "c3238609982efde4e01b99b0caeeb18a"
+  artist = "cher"
+
+  // Artist Search
+  // /2.0/?method=artist.getinfo&artist=Cher&api_key=${this.apiKey}&format=json
+
+  componentDidMount(){
+    fetch(`http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=${this.apiKey}&artist=${this.artist}&album=${this.album}&format=json`)
+      .then( response => response.json() )
+      .then( console.log )
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
